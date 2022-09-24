393d6af2b8473b8b9f041037b26dabec05e739b4
Youseffx <62761039+Youseffx@users.noreply.github.com>
2022-09-24 02:24:45 +0200

    Time: 449 ms (6.81%), Space: 60.6 MB (5.54%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,8 +3,8 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let digits = [];
-    
+    let reverseNum = "";
+    let xCopy = x;
    
     if (x < 0)
         return false;
@@ -12,15 +12,16 @@ var isPalindrome = function(x) {
     {
         let i = 0;
         while(Math.trunc(x) > 0){
-            digits[i]= Math.trunc(x%10);
+            reverseNum += Math.trunc(x%10);
             x /= 10;
             i++;
         } 
+    console.log(reverseNum)
         
-        let Size = digits.length;
-        for(i=0; i < Math.trunc(Size/2); i++)    
-           if (digits[i] !== digits[Size-1-i])
-                  return false;      
+    if (reverseNum == xCopy)   
+        return true;
+    else 
+        return false;
     } 
-    return true;    
+       
 };
