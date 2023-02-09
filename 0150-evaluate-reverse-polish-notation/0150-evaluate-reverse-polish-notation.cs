486b206ff40e23680f3ec5a1b145548a661e8026
itsbahgat <62761039+itsbahgat@users.noreply.github.com>
2023-02-09 13:32:41 +0200

    Time: 97 ms (50.13%), Space: 40.4 MB (24.71%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -1,26 +1,21 @@
 public class Solution {
     public int EvalRPN(string[] tokens) {
+
+        Dictionary <string,Func<int,int,int>> functions = new Dictionary <string,Func<int,int,int>>()
+        {
+            {"+",(x,y)=> x+y },
+            {"*",(x,y)=> x*y },
+            {"/",(x,y)=> x/y },
+            {"-",(x,y)=> x-y }
+            };
         
         Stack<int> stack = new Stack<int>();
-        int x,y,result;
+        int x,y;
         for(int i=0; i<tokens.Length; i++){
-
-
-            if(tokens[i] is "+" or "-" or "*" or "/"){
+            if(functions.ContainsKey(tokens[i])){
                 y = stack.Pop();
                 x = stack.Pop();
-
-                if(tokens[i] is "+") 
-                    result = x + y;
-                else if (tokens[i] is "-")
-                    result = x - y;
-
-                else if (tokens[i] is "*")
-                    result = x * y;
-                else 
-                    result = x / y;
-
-                stack.Push(result);
+                stack.Push(functions[tokens[i]](x,y));
             }
 
             else
